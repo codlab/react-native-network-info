dc8f7e2c99ceb6c6493b8a353173753442c243d2
codlab <codlabtech@gmail.com>
2018-11-23 11:23:55 +0100

    fix override

--- android/src/main/java/com/pusherman/networkinfo/RNNetworkInfoPackage.java
+++ android/src/main/java/com/pusherman/networkinfo/RNNetworkInfoPackage.java
@@ -23,7 +23,6 @@ public final class RNNetworkInfoPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return new ArrayList<>();
     }
